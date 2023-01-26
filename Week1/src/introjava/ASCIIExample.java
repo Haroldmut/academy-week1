111c2b3b8ea4cb955c655d1f6bc23ff0791ed1d8
harold mutobola <harold.mutobola@gmail.ccom>
2023-01-25 23:46:04 -0500

    my first commit

---
+++ Week1/src/introjava/ASCIIExample.java
@@ -0,0 +1,12 @@
+package introjava;
+
+public class ASCIIExample {
+
+	public static void main(String[] args) {
+		char b = 'Z';
+		int base = b;
+		System.out.println("The ASCII value of " + b+ " is: " + base);
+
+	}
+
+}
---
+++ Week1/src/introjava/HellowWorld.java
@@ -0,0 +1,10 @@
+package introjava;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Harold Mutobola");
+	}
+
+}
